c0c5a3ae58425cc20387073dc2722f74f04dca61
aleynaaguneer <80283049+aleynaaguneer@users.noreply.github.com>
2021-03-11 18:51:28 +0300

    Add files via upload

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,10 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+list1=[2,4,6,8,10]
+print("İlk listem:",list1)
+list2=[1,3,5,7,9,11]
+print("İkinci listem:",list2)
+list1.extend(list2)
+print("Birleştirilmiş Yeni Listem:",list1)
+list1= [x*2 for x in (list1)]
+print("Yeni listemin 2 ile çarpılmış hali:",list1)
+for i in list1:
+    print("{} adlı değerin veri tipi: {}".format(i, type(i)))
